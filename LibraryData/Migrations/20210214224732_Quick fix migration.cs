0e21cf8f4bbcefb0e2adc48e521bf91953d50dbf
EVO-PC\andym <evohuntmaster@gmail.com>
2021-02-18 21:36:05 +0200

    Intial push

---
+++ LibraryData/Migrations/20210214224732_Quick fix migration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryData.Migrations
+{
+    public partial class Quickfixmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CheckecIn",
+                table: "CheckoutHistories",
+                newName: "CheckedIn");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CheckedIn",
+                table: "CheckoutHistories",
+                newName: "CheckecIn");
+        }
+    }
+}
